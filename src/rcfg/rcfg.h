f89d61561b47cd166a4557c1e6bdbf653f701203
Dmitry Khominich <dmitry.khominich@exness.com>
2020-07-03 23:15:49 +0300

    print bool as true/false

--- src/rcfg/rcfg.h
+++ src/rcfg/rcfg.h
@@ -42,6 +42,12 @@ namespace rcfg
 		return p;
 	}
 
+	template<>
+	inline std::string ParamTrait<bool>::ToString(const bool & p)
+	{
+		return p ? "true" : "false";
+	}
+
 	struct UpdatableTag {};
 	constexpr UpdatableTag Updatable;
 
